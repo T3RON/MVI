9eb0398d9f4c1b002d29c39d1578d490fae365eb
Luna <luna@MacBook-Pro.local>
2019-05-07 14:51:24 +0430

    info done
    info for movie and serial done

--- app/src/main/java/ir/mseif/app/com/movie/Fragments/CommentsFragment.java
+++ app/src/main/java/ir/mseif/app/com/movie/Fragments/CommentsFragment.java
@@ -1,12 +1,20 @@
 package ir.mseif.app.com.movie.Fragments;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDialog;
+import android.support.v7.widget.AlertDialogLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ir.mseif.app.com.movie.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class CommentsFragment extends Fragment {
     @Override
@@ -16,4 +24,6 @@ public class CommentsFragment extends Fragment {
         //Change R.layout.tab in you classes
         return inflater.inflate(R.layout.fragment_comments, container, false);
     }
+
+
 }
