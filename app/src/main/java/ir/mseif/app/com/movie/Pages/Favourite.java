0ccbc2da02f4138b44b5488035fae973cfcdc3f2
Luna <luna@MacBook-Pro.local>
2019-05-28 17:58:37 +0430

    img rounded added

--- app/src/main/java/ir/mseif/app/com/movie/Pages/Favourite.java
+++ app/src/main/java/ir/mseif/app/com/movie/Pages/Favourite.java
@@ -2,17 +2,36 @@ package ir.mseif.app.com.movie.Pages;
 
 import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ir.basalam.rtlnavigationview.RtlNavigationView;
 import ir.mseif.app.com.movie.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Favourite extends AppCompatActivity {
 
+    @BindView(R.id.grid_my_favourite) GridLayout grid_my_favourite;
+    @BindView(R.id.btn_menu) ImageView btn_menu;
+    @BindView(R.id.nav_view) RtlNavigationView nav_view;
+    @BindView(R.id.drawer_favourite) DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+        ButterKnife.bind(this);
+
+
+        btn_menu.setOnClickListener(v -> drawer.openDrawer(Gravity.RIGHT));
+
     }
     // Font Calligraphy
     @Override
